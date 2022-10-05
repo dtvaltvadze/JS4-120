ed75913ca2ed16bf25d8a9856507cbbfd74b0887
dtvaltvadze <dtvaltvadze@gmail.com>
2022-10-05 12:43:48 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// დავალება:
+// 1.მოცემულია მასივი:
+// Reduce-ის საშუალებით მიიღეთ ერთი კომბინირებული სტრინგი(academy of digital industries);
+
+// let array = ['academy', 'of', 'digital', 'industries'].reduce(function(accumulator, currentvalue) {
+//     return accumulator + ' ' .concat(currentvalue);
+// });
+// console.log(array);
+
+
+
+// 2. მოცემულია მასივი: 
+// Sort მეთოდის საშუელებით დაალაგეთ ელემენტები კლებადონის მიხედვით და ამოიღეთ მინიმალური რიცხვი;
+
+// let array = [23,45,32,5,87,7,3,98].sort( ( x, y) => y - x);
+
+// let minValue = array.sort((x, y) => x - y)[0];
+
+// console.log(array);
+// console.log(minValue);
+
+
+
+
